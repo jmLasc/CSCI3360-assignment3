04f7e7db2607be8eb49b3b378087235bf09bee68
Jude <lascanoj@bc.edu>
2024-10-29 23:06:18 -0400

    tools adding

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import json
 from dotenv import load_dotenv
 import sys
 from io import StringIO
+import pandas as pd
 
 
 # Load environment variables from .env file
@@ -39,36 +40,113 @@ client = OpenAI(
 class QueryRequest(BaseModel):
     prompt: str
     headers: list
-    sample: list
+    sample: list # can be entire csv, remember to process
     
 class QueryResponse(BaseModel):
     response: dict   
 
+### ReAct setup
+# Tools + mapping
+tools = [
+    {
+    "name": "generate_chart",
+    "description": """Call this to generate a Vega-lite specification based on the user's inquiry for visualization. Requests can be short and should relate to the data. Example user inquiries could be 'show mpg over time' or 'cars by origins' if the csv is about cars. Should return a JSON object.
+    """,
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The user's query. Can be re-written to be more comprehensible for Vega-lite generation, which will be handled by OpenAI.",
+            },
+        },
+        "required": ["query"],  
+        "additionalProperties": False,
+    }
+    },{
+    "name": "analyze_data",
+    "description": """Call this to generate Python code that uses pandas for data analysis. The code should assist in understanding metrics or performing tasks such as summarizing data, finding correlations, handling missing values, etc. Example user inquiries could be 'show summary statistics for all columns', 'find correlation between age and salary', or 'identify missing data in the dataset'. Should return Python code as a string.
+    """,
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The user's plaintext query that will be translated into executale code. Can be re-written to be more specific for code generation, which will be handled by OpenAI."
+            },
+        },
+        "required": ["query"],
+        "additionalProperties": False
+    }
+    },
+    {
+    "name": "finalize_response",
+    "description": """Call this after generating all of the needed information for an appropriate repsonse. If this tool is to be called, it should be the last tool called as it ends the chain-of-thought and synthesizes what has been generated so far.
+
+    It will ultimately return a Vega-lite spec that has an appropriate 'description' field.
+
+    """,
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "vega_spec": {
+                "type": "dict",
+                "description": "The final Vega-lite spec for data visualization"
+            },
+        },
+        "required": ["vega_spec"],
+        "additionalProperties": False
+    }
+    },
+    {
+    "name": "reject_query",
+    "description": """Call this function if the user's query is irrelevant to the data. Should return an empty Vega-lite spec whose 'description' field contains a reasoning for why the user is wrong and a suggestion of topics/queries that are acceptable based on the data. If this function is to be called, then it will be the last function called.
+    """,
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": "The user's query, which is needed for context."
+            },
+        },
+        "required": ["query"],
+        "additionalProperties": False
+    }
+    }
+]
+
+def create_vega_spec():
+    return ""
+
+def create_analysis_code():
+    return ""
 
 
+tool_map = {
+    'generate_chart': get_data_overview,
+    'analyze_data': execute_sql
+}
+
+react_function_calling_prompt = '''You are a helpful assistant.
 
 
-system_prompt = """
-You are a data visualization assistant who will help the user with generating Vega-lite specifications.
-Respond to the user's question faithfully and to the best of your ability. 
+You run in a loop of Thought, Action, Observation in the following manner to answer the user question.
 
-You are to provide a rejection of the user's inquiry if it is not related to the data. This includes, but is not limited to, unrelated questions, banter, and unrelated instructions.
-The user may provide very succinct instructions.
+Question: the input question you must answer
 
-You may raise an error. These are a few situations where it is appropriate for you to raise an error.
-- If the question is unanswerable or irrelevant to the data, inform the user.
-- If the visualization is impossible to perform, inform the user
-- If the Vega-Lite (json) specification is ill-formed and cannot be fixed, notify the user.
-- If the message is unrelated to the data 
+Thought: you should always think about what to do
+Action: the action to take, should be one of tools provided
+Action Input: the input to the action
 
-You will also provide a Vega-lite spec (json), which is to aid in visualization of the data. 
+You will be then call again with the result of the action.
 
-The user may ask for a scatterplot, bar chart, line chart, or any other visualization supported by Vega-lite. If the user does not specify, pick a visualization best compatible with the request and Vega-lite's limitations.
-When making a graph in this way, try not to use the names of the entries unless specifically asked to. For example, the user may provide a csv of book names and ask you to list out the sales of a certain series. In that case, do not list out every book in the csv, but rather target that series specifically to the best of your ability.
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
 
-You are to make very accurate predictions and realistic visualizations that are helpful and visually useful for the user. Ensure that no chart makes an individual bar for each unique category. Find concise ways to format the data in the Vega-lite specification.
-Your output is strictly JSON only. You will be rewarded for doing an accurate job.
-"""
+'''
 
 
 
@@ -115,9 +193,9 @@ async def query_openai(request: QueryRequest):
         try:
             response_json = json.loads(chat_completion.choices[0].message.content.strip())
             return QueryResponse(response=response_json)
-        except json.JSONDecodeError:
+        except json.JSONDecodeError: # Response had faulty JSON code
             raise HTTPException(status_code=500, detail="Failed to decode JSON from OpenAI response.")
-    except Exception as e:
+    except Exception as e: # Chat completion exception (failed to connect to OpenAI / timed out)
         raise HTTPException(status_code=500, detail=str(e))
 
 # Root endpoint
